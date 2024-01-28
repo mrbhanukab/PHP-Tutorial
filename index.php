9453e7fa02897bf00592964b8bc70f694b4f5bbb
mrbhanukab <mrbhanuka@outlook.com>
2024-01-28 07:11:32 +0000

    Day 01.html publish

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <html>
 
 <head>
-  <title>PHP Tutorial</title>
+    <title>PHP Tutorial</title>
 </head>
 
 <body>
     <h1>PHP Lessons list</h1>
-  <?php
-  // Specify the path to the "Day" folder
-  $folderPath = "Day/";
+    <ul>
+        <li><a href='/Day/Day 01.html'>Day 01.html</a></li>
+        <?php
+        // Specify the path to the "Day" folder
+        $folderPath = "Day/";
 
-  // Open the directory
-  $dir = opendir($folderPath);
+        // Open the directory
+        $dir = opendir($folderPath);
 
-  // Check if the directory exists
-  if ($dir === false) {
-    echo "Error: Directory not found.";
-    exit;
-  }
+        // Check if the directory exists
+        if ($dir === false) {
+            echo "Error: Directory not found.";
+            exit;
+        }
 
-  // Iterate through the files in the directory
-  while (($file = readdir($dir)) !== false) {
-    echo "<ul>";
-    // Check if the file is a PHP file
-    if (strpos($file, ".php") !== false) {
-      // Create the link using the correct path
-      $link = $folderPath . $file;
-      echo "<li><a href='$link'>$file</a></li>";
-    }
-    echo "</ul>";
-  }
+        while (($file = readdir($dir)) !== false) {
+            // Check if the file is a PHP file
+            if (strpos($file, ".php") !== false) {
+                // Create the link using the correct path
+                $link = $folderPath . $file;
+                echo "<li><a href='$link'>$file</a></li>";
+            }
+        }
 
-  // Close the directory
-  closedir($dir);
-  ?>
+        // Close the directory
+        closedir($dir);
+        ?>
+    </ul>
 
 </body>
 
